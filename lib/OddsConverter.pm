e8caaa89a127648bd837f455d923d9f5602d0b77
Anbalagan (anbu) Pugaleesan <anbu78@gmail.com>
2012-10-10 03:05:09 +0530

    Odds Converter
    This perl module converts given probablity to decimal odds and roi

--- lib/OddsConverter.pm
+++ lib/OddsConverter.pm
@@ -1,5 +1,74 @@
 package OddsConverter;
 
+use Modern::Perl;
+use POSIX qw (ceil);
+
+use autodie;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'ProbabilityType',
+    as 'Num',
+    where { $_ >= 0 && $_ <= 1 },
+    message { 'probability value should be >= 0 and <= 1' };
+
+has 'probability' => (
+             is  => 'rw',
+             isa => 'ProbabilityType',
+             required => 1
+         );
+
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+
+    if ( @_ == 2 && !ref $_[0] ) {
+        return $class->$orig( $_[0] => $_[1] );
+    } else {
+        return $class->$orig(@_);
+    }
+};
+
+sub decimal_odds
+{
+    my $self = shift;
+    my $odds;
+
+    if ( $self->probability == 0 ) {
+        $odds = 'Inf.';
+    } else {
+        $odds = sprintf("%0.2f", $self->calc_odds);
+    }
+
+    return $odds;
+}
+
+sub roi
+{
+    my $self = shift;
+    my $roi;
+
+    if ( $self->probability == 0 ) {
+        $roi = 'Inf.';
+    } else {
+        $roi = sprintf("%d%%", $self->calc_roi);
+    }
+
+    return $roi;
+}
+
+sub calc_odds
+{
+    my $self = shift;
+    return (1 / $self->probability) if ($self->probability != 0);
+}
+
+sub calc_roi
+{
+    my $self = shift;
+    return ceil(($self->calc_odds - 1) * 100) if ($self->probability != 0);
+}
+
 =head1 NAME
 
 OddsConverter
